18067923cd830e9f27d27b4e2a94682298aef2aa
Mitsuki Fukunaga <Mitsuki.Fukunaga@caresuper.com.au>
2025-12-18 23:49:30 +1100

    chore: Release v0.2.1

--- src/options/Options.jsx
+++ src/options/Options.jsx
@@ -444,7 +444,7 @@ export default function Options() {
           <span>GitHub</span>
         </a>
         <span>•</span>
-        <span>v0.2.0</span>
+        <span>v0.2.1</span>
         <span>•</span>
         <a
           href="https://github.com/mtskf/Snooooze/issues"
