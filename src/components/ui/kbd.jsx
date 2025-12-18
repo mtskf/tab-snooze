d43f9be86d8b4ec425ae7853eed2b08f59a36f9f
Mitsuki Fukunaga <Mitsuki.Fukunaga@caresuper.com.au>
2025-12-18 13:46:58 +1100

    style: reduce Kbd size slightly

--- src/components/ui/kbd.jsx
+++ src/components/ui/kbd.jsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 const Kbd = React.forwardRef(({ className, ...props }, ref) => (
   <kbd
     className={cn(
-      "pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-sans text-[10px] font-medium text-muted-foreground opacity-100",
+      "pointer-events-none inline-flex h-4 select-none items-center gap-1 rounded border bg-muted px-1 font-sans text-[9px] font-medium text-muted-foreground opacity-100",
       className
     )}
     ref={ref}
